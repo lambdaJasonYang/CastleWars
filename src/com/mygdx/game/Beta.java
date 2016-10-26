682c3e7c03d9e4872b01c7542197b37bf6ef0a59
jy1486 <jy1486@nyu.edu>
2016-10-25 22:52:05 -0400

    Update Beta.java
    Achievements code and variables deleted for public viewing

--- src/com/mygdx/game/Beta.java
+++ src/com/mygdx/game/Beta.java
@@ -5,20 +5,7 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Screen;
 
 public class Beta extends Game implements ApplicationListener{
-	String a1 = "CggIzI7o5gUQAhAB"; //noob
-	String a2 = "CggIzI7o5gUQAhAC"; //mill
-	String a3 = "CggIzI7o5gUQAhAD";//wiz
-	String a4 = "CggIzI7o5gUQAhAE";//sum
-	String a5 = "CggIzI7o5gUQAhAF";//mriwant
-	String a6 ="CggIzI7o5gUQAhAG";//td
-	String a7 ="CggIzI7o5gUQAhAH";//resili
-	String a8= "CggIzI7o5gUQAhAI";//allmy
-	String a9="CggIzI7o5gUQAhAJ";//alilless
-	String a10="CggIzI7o5gUQAhAK";//novice
-	String a11="CggIzI7o5gUQAhAL";//true dis
-	String a12="CggIzI7o5gUQAhAM";//gobroke
-	String l1 ="CggIzI7o5gUQAhAN";
-	String l2 ="CggIzI7o5gUQAhAO";
+
 	datas save;
 	shop shoppin;
 	mainmenu meta;	
